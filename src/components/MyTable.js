f04abdb9aa4b7a2bb4de7288e30bee9d78c0f541
shibinjohn88 <shibinjohnpdlm@gmail.com>
2024-07-17 20:10:51 -0400

    api moved to environment variables

--- src/components/MyTable.js
+++ src/components/MyTable.js
@@ -10,7 +10,7 @@ function MyTable() {
     useEffect(() => {
         const fetchData = async () => {
           try {
-            const response = await fetch(`http://localhost:3001/api/students`);
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/api/students`);
             const data = await response.json();
             setStudentList(data)
           } catch (error) {
